3ffbc830865647d5e54a8a4f2ef7a446991b9b26
emyvictoria061003@gmail.com <guto9201@gmailcom>
2024-08-29 16:31:42 -0300

    feat: Implementa a Soma
    - O funcionamento se da por meio de uma linha de comando

---
+++ SomarDoisNumeros.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SomarDoisNumeros {
+    public static void main(String[] args) {
+        //Declaração de variaveis
+        Scanner leitor = new Scanner(System.in);
+        int a, b, resultadoDaSoma;
+        
+        //Entrada de Dados
+        System.out.println("Digite um valor: ");
+        a = leitor.nextInt();
+
+        System.out.println("Digite outro valor: ");
+        b = leitor.nextInt();
+
+        //Processamento
+        resultadoDaSoma = a + b;
+        
+        //Saída
+        //System.out.println(a + "+" + b + "=" + resultadoDaSoma);
+        System.out.printf("%d + %d = %d\n", a, b, resultadoDaSoma);
+    }
+}
