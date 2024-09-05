b342ca2a863c89a3e35cf0efd90d9a278ea71213
emyvictoria061003@gmail.com <guto9201@gmailcom>
2024-09-05 15:36:43 -0300

    teste: teste no codigo SomarDoisNumeros
    Signed-off-by: emyvictoria061003@gmail.com <guto9201@gmailcom>

--- ENTRADA_SAIDA/SomarDoisNumeros.java
+++ ENTRADA_SAIDA/SomarDoisNumeros.java
@@ -1,23 +1,24 @@
 import java.util.Scanner;
 
-public class SomarDoisNumeros {
+public class SomarDoisNumeros{
     public static void main(String[] args) {
-        //Declaração de variaveis
-        Scanner leitor = new Scanner(System.in);
-        int a, b, resultadoDaSoma;
-        
-        //Entrada de Dados
-        System.out.println("Digite um valor: ");
-        a = leitor.nextInt();
+        try (//Declaração de variaveis
+        Scanner leitor = new Scanner(System.in)) {
+            int a, b, resultadoDaSoma;
+            
+            //Entrada de Dados
+            System.out.println("Digite um valor: ");
+            a = leitor.nextInt();
 
-        System.out.println("Digite outro valor: ");
-        b = leitor.nextInt();
+            System.out.println("Digite outro valor: ");
+            b = leitor.nextInt();
 
-        //Processamento
-        resultadoDaSoma = a + b;
-        
-        //Saída
-        //System.out.println(a + "+" + b + "=" + resultadoDaSoma);
-        System.out.printf("%d + %d = %d\n", a, b, resultadoDaSoma);
+            //Processamento
+            resultadoDaSoma = a + b;
+            
+            //Saída
+            //System.out.println(a + "+" + b + "=" + resultadoDaSoma);
+            System.out.printf("%d + %d = %d\n", a, b, resultadoDaSoma);
+        }
     }
 }
