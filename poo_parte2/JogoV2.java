99dd876d4e92c7e2918d34a0a5dd5ad5a5818cae
emyvictoria061003@gmail.com <aluno@IPIRANGA.FATEC>
2024-09-19 17:53:29 -0300

    fix: consertar erro ao adicionar o poo_parte2

--- poo_parte2/JogoV2.java
+++ poo_parte2/JogoV2.java
@@ -1,9 +1,52 @@
+import java.util.Random;
+
 public class JogoV2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        System.out.println("Comecando..");
+
         var cacador = new Personagem();
         cacador.nome = "HeroBrine";
+
+        var soneca = new Personagem(2, 4, 10);
+        soneca.nome = "UrsinhoPooh";
+        
+        var fominha = new Personagem(6, 10, 2);
+        fominha.nome = "Scooby";
+        
+        var gerador = new Random();
+
         while (true) {
-            cacador.cacar();
+            var AcaoDoCacador = gerador.nextInt(3) + 1;
+            switch(AcaoDoCacador){
+                case 1 -> cacador.cacar();
+                case 2 -> cacador.comer();
+                case 3 -> cacador.dormir();
+            }
+            System.out.println(cacador);
+            System.out.println("-------------------------------------");
+
+            var AcaoDoSoneca = gerador.nextInt(3) + 1;
+            
+            if(AcaoDoSoneca == 1){
+                soneca.comer();
+            } 
+            else if(AcaoDoSoneca == 2){
+                soneca.dormir();
+            } 
+            else {
+                soneca.cacar();
+            }
+            System.out.println(soneca);
+            System.out.println("-------------------------------------");
+
+            switch(gerador.nextInt(3) + 1) {
+                case 1 -> fominha.cacar();
+                case 2 -> fominha.comer();
+                default -> fominha.dormir();
+            }
+            System.out.println(fominha);
+            System.out.println("================================");
+            Thread.sleep(5000);
         }
     }
    
--- poo_parte2/Personagem.java
+++ poo_parte2/Personagem.java
@@ -1,5 +1,3 @@
-import javax.swing.JOptionPane;
-
 public class Personagem{
     String nome;
     private int energia;
@@ -7,24 +5,29 @@ public class Personagem{
     private int sono;
 
     Personagem(){
-        System.out.println("Contruindo
-        novo Personagem");
+        System.out.println("Contruindo novo Perosnagem");
         energia = 10;
         fome = 0;
         sono = 0;
     }
+
+    Personagem(int energia, int fome, int sono) {
+        System.out.println("Contruindo novo Perosnagem");
+        this.energia = energia < 0 || energia > 10 ? 10 : energia;
+        this.fome = fome >= 0 && fome <= 10? fome : 0;
+        this.sono = sono >= 0 && sono <= 10? sono : 0;
+    }
  
     void cacar(){
         if(energia >=2){
-            System.out.printf("%s caçando\n", nome);
+            System.out.printf("%s cacando\n", nome);
             energia -= 2; //energia = energia - 2
         }else{
-            System.out.printf("%s sem energia para caçar", nome);
-            if(fome < 10)
-                fome = fome + 1; // fome +=1 fome++ fome++
-            sono = sono == 10 ? sono : sono + 1;
+            System.out.printf("%s sem energia para cacar\n", nome);
+            
         }
-       
+       if(fome < 10) fome = fome + 1; // fome +=1 fome++ fome++
+        sono = sono == 10 ? sono : sono + 1;
     }
    
     //método comer
@@ -49,5 +52,9 @@ public class Personagem{
             System.out.println(nome + " sem sono");
         }
        
+    }
+
+    public String toString(){
+        return String.format("%s: e:%d, f:%d, s:%d",nome, energia, fome, sono);
     }
 }
